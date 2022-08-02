a3f9416a8f5fa94dcf0a32a2e2bfdd5aa2e546c6
Jacob Pattison <94932754+JacobPattison@users.noreply.github.com>
2022-08-02 19:23:59 +0100

    Add files via upload

---
+++ Jarvis/Jarvis/Jarvis.Designer.cs
@@ -0,0 +1,83 @@
+﻿namespace Jarvis
+{
+    partial class Jarvis
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.commandsListBox = new System.Windows.Forms.ListBox();
+            this.speakingTimer = new System.Windows.Forms.Timer(this.components);
+            this.speechOutputTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // commandsListBox
+            // 
+            this.commandsListBox.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.commandsListBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.commandsListBox.ForeColor = System.Drawing.Color.White;
+            this.commandsListBox.FormattingEnabled = true;
+            this.commandsListBox.ItemHeight = 16;
+            this.commandsListBox.Location = new System.Drawing.Point(0, 0);
+            this.commandsListBox.Margin = new System.Windows.Forms.Padding(4);
+            this.commandsListBox.Name = "commandsListBox";
+            this.commandsListBox.Size = new System.Drawing.Size(839, 527);
+            this.commandsListBox.TabIndex = 0;
+            this.commandsListBox.Visible = false;
+            // 
+            // speakingTimer
+            // 
+            this.speakingTimer.Interval = 1000;
+            this.speakingTimer.Tick += new System.EventHandler(this.speakingTimer_Tick);
+            // 
+            // speechOutputTimer
+            // 
+            this.speechOutputTimer.Enabled = true;
+            this.speechOutputTimer.Tick += new System.EventHandler(this.speechOutputTimer_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(839, 527);
+            this.Controls.Add(this.commandsListBox);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "Jarvis";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox commandsListBox;
+        private System.Windows.Forms.Timer speakingTimer;
+        private System.Windows.Forms.Timer speechOutputTimer;
+    }
+}
+
---
+++ Jarvis/Jarvis/Jarvis.cs
@@ -0,0 +1,179 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+using System.Diagnostics;
+//using SpotifyAPI.Web;
+
+namespace Jarvis
+{
+    public partial class Jarvis : Form
+    {
+        SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+        SpeechSynthesizer jarvis = new SpeechSynthesizer();
+        SpeechRecognitionEngine startListening = new SpeechRecognitionEngine();
+        Random random = new Random();
+        int RecTimeOut = 0;
+        DateTime timeNow = DateTime.Now;
+        public Jarvis()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _recognizer.SetInputToDefaultAudioDevice();
+            _recognizer.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"DefaultCommands.txt")))));
+            _recognizer.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(Default_SpeechRecognized);
+            _recognizer.SpeechDetected += new EventHandler<SpeechDetectedEventArgs>(_recognizer_SpeechRecognized);
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+
+            startListening.SetInputToDefaultAudioDevice();
+            startListening.LoadGrammarAsync(new Grammar(new GrammarBuilder(new Choices(File.ReadAllLines(@"DefaultCommands.txt")))));
+            startListening.SpeechRecognized += new EventHandler<SpeechRecognizedEventArgs>(startListening_SpeechRecognized);
+        }
+
+        string speechOutput;
+        public void GameMode()
+        {
+            Process.Start(@"C:\Program Files (x86)\Steam\steam.exe");
+            Process.Start(@"C:\Program Files (x86)\Epic Games\Launcher\Portal\Binaries\Win32\EpicGamesLauncher.exe");
+            Process.Start(@"C:\Users\badgo\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\Discord.lnk");
+        }
+
+        public void CodeMode()
+        {
+            Process.Start(@"C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\IDE\devenv.exe");
+            Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe");
+        }
+
+        private void Default_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            int randomNumber;
+            string speech = e.Result.Text;
+            speechOutput = speech;
+            if (speech == "Jarvis Open youtube")
+            {
+                jarvis.SpeakAsync("Opening Youtube");
+                Process.Start("https://www.youtube.com");
+            }
+            if (speech == "Jarvis Open chrome")
+            {
+                jarvis.SpeakAsync("Opening google chrome");
+                Process.Start(@"C:\Program Files\Google\Chrome\Application\chrome.exe");
+            }
+            if (speech == "Jarvis Open discord")
+            {
+                jarvis.SpeakAsync("Opening discord");
+                Process.Start(@"C:\Users\badgo\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Discord Inc\Discord.lnk");
+            }
+            if (speech == "Jarvis Open fall guys")
+            {
+                jarvis.SpeakAsync("Opening fall guys");
+                Process.Start(@"C:\\Program Files\\Epic Games\\FallGuys\\FallGuys_client.exe");
+            }
+            if (speech == "Jarvis Open powerwash simulator")
+            {
+                jarvis.SpeakAsync("Opening powerwash simulator");
+                Process.Start(@"C:\\Program Files (x86)\\Steam\\steamapps\\common\\PowerWash Simulator\\PowerWashSimulator.exe");
+            }
+            if (speech == "Jarvis Open raindbow six siege")
+            {
+                jarvis.SpeakAsync("Opening raindbow six siege");
+                Process.Start(@"C:\Program Files (x86)\Steam\steamapps\common\Tom Clancy's Rainbow Six Siege\\RainbowSix.exe");
+            }
+            if (speech == "Jarvis Game mode")
+            {
+                jarvis.SpeakAsync("Executing game mode");
+                GameMode();
+            }
+            if (speech == "Jarvis Code mode")
+            {
+                jarvis.SpeakAsync("Executing code mode");
+                CodeMode();
+            }
+            if (speech == "Jarvis What time is it")
+            {
+                jarvis.SpeakAsync(DateTime.Now.ToString("h mm tt"));
+            }
+            if (speech == "Jarvis Stop talking")
+            {
+                jarvis.SpeakAsyncCancelAll();
+                randomNumber = random.Next(1, 2);
+                if (randomNumber == 1)
+                {
+                    jarvis.SpeakAsync("Yes Sir");
+                }
+                if (randomNumber == 2)
+                {
+                    jarvis.SpeakAsync("I am sorry i will be quiet");
+                }
+            }
+            if (speech == "Jarvis Sleep")
+            {
+                _recognizer.RecognizeAsyncCancel();
+                startListening.RecognizeAsync(RecognizeMode.Multiple);
+            }
+
+            if (speech == "Jarvis Show commands")
+            {
+                string[] commands = (File.ReadAllLines(@"DefaultCommands.txt"));
+                commandsListBox.Items.Clear();
+                commandsListBox.SelectionMode = SelectionMode.None;
+                commandsListBox.Visible = true;
+                foreach (string command in commands)
+                {
+                    commandsListBox.Items.Add(command);
+                }
+            }
+            if (speech == "Jarvis Hide commands")
+            {
+                commandsListBox.Visible = false;
+            }
+        }
+
+        private void _recognizer_SpeechRecognized(object sender, SpeechDetectedEventArgs e)
+        {
+            RecTimeOut = 0;
+        }
+
+        private void startListening_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string speech = e.Result.Text;
+
+            if(speech == "Jarvis Wake up")
+            {
+                startListening.RecognizeAsyncCancel();
+                jarvis.SpeakAsync("I am here");
+                _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            }
+        }
+
+        private void speakingTimer_Tick(object sender, EventArgs e)
+        {
+            if (RecTimeOut == 10)
+            {
+                _recognizer.RecognizeAsyncCancel();
+            }
+            else if (RecTimeOut == 11)
+            {
+                speakingTimer.Stop();
+                startListening.RecognizeAsync(RecognizeMode.Multiple);
+                RecTimeOut = 0;
+            }
+        }
+
+        private void speechOutputTimer_Tick(object sender, EventArgs e)
+        {
+            Console.WriteLine(speechOutput);
+        }
+    }
+}
